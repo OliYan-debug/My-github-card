734961d48c4d14c531b8393b9ab6026e92c5fd50
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2022-11-18 20:35:10 -0300

    fixing placeholder from input

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -6,7 +6,7 @@ function UserForm(props) {
   return (
     <div className="mb-3 me-lg-5">
       <h3>Search your profile</h3>
-      <input className="p-2" placeholder="username" type="text" onChange={props.handleUser} />
+      <input className="p-2" placeholder="Github Username" type="text" onChange={props.handleUser} />
       <button id="btn" className="ms-2 rounded btn bg-dark text-white " onClick={props.changeUser}>Change</button>
       
     </div>
