5d624f02b1ec4c0341808f413d427299352c9d8f
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2022-11-18 20:33:24 -0300

    Fixing navbar title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar(){
     return(
     <nav className="w-100 p-3 navbar navbar-dark bg-dark justify-content-center">
-        <a href="/" className="navbar-brand">My Github Card</a>
+        <h1><a href="/" className="navbar-brand">My Github Card</a></h1>
     </nav>
     )
 }
